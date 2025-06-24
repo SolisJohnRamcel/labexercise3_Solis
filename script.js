5dcb2c4609bf0fd1952255602b050a42700269ee
SolisJohnRamcel <ramcelblitzz@gmail.com>
2025-06-24 15:06:55 +0800

    Added index.html styles.css script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+window.onload = function () {
+  console.log("Page loaded successfully!");
+};
+
+function changeText() {
+  document.getElementById("message").textContent = "You clicked the button!";
+}
